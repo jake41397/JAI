5746ab95d6a7a9f1f96ec885664f435a06bbcabe
Canute <jake41397@gmail.com>
2019-02-19 03:39:15 -0500

    built commit for real

---
+++ build/precache-manifest.7355fc8e84233fb88af8c846c6fcc24e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "341f838a5376d671af95",
+    "url": "/static/js/main.341f838a.chunk.js"
+  },
+  {
+    "revision": "34809ba241faba506830",
+    "url": "/static/js/2.34809ba2.chunk.js"
+  },
+  {
+    "revision": "341f838a5376d671af95",
+    "url": "/static/css/main.12daa04f.chunk.css"
+  },
+  {
+    "revision": "a0901bad3796f73d012391b67470a0be",
+    "url": "/index.html"
+  }
+];
